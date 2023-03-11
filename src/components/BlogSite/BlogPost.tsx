e65ed4d097a572ce1ba5b5513234d2e2ec808ec8
Marcel <zseg1ipg.mendrok.marcel@gmail.com>
2023-03-12 00:00:05 +0100

    Component BlogSite working properly

--- src/components/BlogSite/BlogPost.tsx
+++ src/components/BlogSite/BlogPost.tsx
@@ -1,26 +1,30 @@
 import React from "react";
+import {Review_Record} from "types";
 
-export const BlogPost = () => {
+interface Props {
+    review: Review_Record;
+}
+
+export const BlogPost = (props: Props) => {
+    const {review} = props;
     return (
         <div className="blog-post">
             <div className="blog-post_img">
                 <img
-                    src="https://images.unsplash.com/photo-1612287230202-1ff1d85d1bdf?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTU3fHx0ZWNobm9sb2d5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-                    alt=""/>
+                    src={review.background_image}
+                    alt={review.name + " theme image"}
+                />
             </div>
             <div className="blog-post_info">
+                {/*@TODO blog_post_date*/}
                 <div className="blog-post_date">
                     <span>Sagar Developer</span>
                     <span>Nov 12 2021</span>
                 </div>
-                <h1 className="blog-post_title">Lorem ipsum dolor sit amet.</h1>
+                <h1 className="blog-post_title">{review.name}</h1>
                 <p className="blog-post_text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores a, tempore veniam quasi sint
-                    fugiat
-                    facilis, facere, amet magnam optio velit. Laudantium et temporibus soluta, esse cupiditate
-                    aliquid
-                    dicta
-                    accusantium.
+                    {/*@ts-ignore*/}
+                    {[...review.review].slice(0, 300).join("") + "..."}
                 </p>
                 <a href="#" className="blog-post_cta">Read More</a>
             </div>
--- src/components/BlogSite/BlogSite.tsx
+++ src/components/BlogSite/BlogSite.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./BlogSite.css"
 import {BlogPost} from "./BlogPost";
+import {Review_Record} from "types";
 
 export const BlogSite = () => {
+    const [reviewsList, setReviewsList] = useState<Review_Record[] | null>(null);
+    useEffect(() => {
+        (async () => {
+            const response = await fetch("http://localhost:3001/review");
+            setReviewsList(await response.json());
+        })()
+    }, [])
     return (
-        <div className="blog-container">
-            <h2>Posts</h2>
-            <BlogPost/>
+        <div className="container">
+            <h2>Posts </h2>
+            {
+                reviewsList ?
+                    reviewsList.map(review => <BlogPost key={review.id} review={review}/>) :
+                    "LOADING..."
+            }
         </div>
     )
 }
